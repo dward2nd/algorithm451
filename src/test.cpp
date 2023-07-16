204d0d417c4a4113436f166b6a767b6892c6a493
Saksinkarn Petchkuljinda <dward.the.2nd@gmail.com>
2023-07-16 14:31:50 +0700

    Format src/test.cpp

--- src/test.cpp
+++ src/test.cpp
@@ -11,9 +11,9 @@ void hwtest() {
         ios::in); // open a file to perform read operation using file object
     if (newfile.is_open()) { // checking whether the file is open
         string tp;
-        while (getline(
-            newfile,
-            tp)) { // read data from file object and put it into string.
+        while (
+            getline(newfile,
+                    tp)) { // read data from file object and put it into string.
             cout << tp << "\n"; // print the data of the string
         }
         newfile.close(); // close the file object.
